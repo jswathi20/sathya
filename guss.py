9078998b8c4ead27937a7d9f9d9aefcfbc9152b8
jswathi20 <60419120+jswathi20@users.noreply.github.com>
2020-01-29 14:17:00 +0530

    Create guss.py

---
+++ guss.py
@@ -0,0 +1,14 @@
+import random
+n = 10
+guess = int(input("Enter an integer from 1 to 99: "))
+while n != ("guess"):
+    print
+    if guess < n:
+        print ("guess is wrong")
+        guess = int(input("Enter an integer from 1 to 99: "))
+    elif guess > n:
+        print ("guess is wrong")
+        guess = int(input("Enter an integer from 1 to 99: "))
+    else:
+        print ("you guessed it!")
+        break
