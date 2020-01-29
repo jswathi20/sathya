614955427f1e19c6f7ca28c839bd9bc8cfc16494
jswathi20 <60419120+jswathi20@users.noreply.github.com>
2020-01-29 12:40:34 +0530

    Create billing.py

---
+++ billing.py
@@ -0,0 +1,12 @@
+def calculate_fare():
+  distance = int(input("Enter the distance: "))
+  name = str(input("Enter the name: "))
+  source destination = input("enter the source destination place")
+  final destination  = input("enter the final destination place")
+   return fare
+    def print_fare(fare):
+    print("%0s%.2f" % ("Fare is: $", fare))
+    def main():
+        fare = calculate_fare()
+        print_fare(fare)
+  
