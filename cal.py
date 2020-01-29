b0abf10ceb3eaaef8dd21bd6eb97f2efce970181
jswathi20 <60419120+jswathi20@users.noreply.github.com>
2020-01-29 10:55:37 +0530

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,17 @@
+def add(x,y):
+        return x+y;
+def sub(x,y):
+        return x-y;
+def mul(x,y):
+        return x*y;
+def div(x,y):
+        return x/y;
+print("choice= 1,2,3,4")
+print("add",add(2,3))
+print("sub",sub(2,3))
+print("mul",mul(2,3))
+print("div",div(2,3))
+
+
+
+    
